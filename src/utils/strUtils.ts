2d9d93ebba86ce88c28bec9f0730ef1a39bbd902
YangQi <70502828+YangFong@users.noreply.github.com>
2022-01-07 21:42:03 +0800

    style: add prettier and format code

--- src/utils/strUtils.ts
+++ src/utils/strUtils.ts
@@ -1,32 +1,33 @@
 function objFormatStr(params, lIndex = 1) {
-    let string = `${lIndex === 1 ? '\n' : ''}{`
-    const tabStr = '  '
-    const array = Object.keys(params)
+    let string = `${lIndex === 1 ? '\n' : ''}{`;
+    const tabStr = '  ';
+    const array = Object.keys(params);
     for (let i = 0; i < array.length; i++) {
         const key = array[i];
         if (typeof params[key] === 'object') {
-            string += `\n${stringMul(tabStr, lIndex)}${key}: ${objFormatStr(params[key], lIndex + 1)}`
+            string += `\n${stringMul(tabStr, lIndex)}${key}: ${objFormatStr(
+                params[key],
+                lIndex + 1
+            )}`;
         } else {
-            string += `\n${stringMul(tabStr, lIndex)}${key}: ${params[key]}`
+            string += `\n${stringMul(tabStr, lIndex)}${key}: ${params[key]}`;
         }
-
     }
-    string += `\n${stringMul(tabStr, lIndex - 1)}}`
-    return string
+    string += `\n${stringMul(tabStr, lIndex - 1)}}`;
+    return string;
 }
 
-export function stringMul(params, len,) {
+export function stringMul(params, len) {
     if (len <= 0) {
-        return ''
+        return '';
     }
-    let str = ''
+    let str = '';
     for (let i = 0; i < len; i++) {
-        str += params
+        str += params;
     }
-    return str
+    return str;
 }
 
 export function stringMulLastEnd(params, len, targetStr) {
-
-    return stringMul(params, len) + targetStr
+    return stringMul(params, len) + targetStr;
 }
