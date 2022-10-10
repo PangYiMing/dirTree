75a297d4fc7ec80bad821ff5351e28925a42ac05
ympang <ympang@trip.com>
2022-10-10 20:55:51 +0800

    清空文件夹

--- src/fileDirTree/index.ts
+++ src/fileDirTree/index.ts
@@ -3,7 +3,7 @@
  * @Date: 2022-01-10 09:48:34
  * @LastEditors: pym
  * @Description:
- * @LastEditTime: 2022-10-10 20:49:47
+ * @LastEditTime: 2022-10-10 20:55:32
  */
 import path from 'path';
 import { walkSync } from '../utils/readDirPaths';
@@ -11,10 +11,10 @@ import transformDocs from '../plugin/index';
 import { execAbortArr, strNotIncludeStringInArr } from '../utils/utils';
 
 import { Command } from 'commander';
+import fs from 'fs';
 
 // TODO 支持合并成一个文件
 // TODO 支持文件路径
-// TODO 清空文件夹
 const program = new Command();
 program
     .option('-t, --target <type>', 'specify target path')
@@ -51,6 +51,16 @@ let abortArr: string[] = execAbortArr(
 
 let isFirst = true;
 const target = options.target ? path.resolve(options.target) : process.cwd();
+const outFilePath = path.resolve(target, 'docs');
+
+// 清空文件夹
+walkSync({
+    dirPath: outFilePath,
+    callback: (filePath, stat, treeOpt: { deep; isEnd; parent }) => {
+        fs.unlinkSync(filePath);
+    },
+    maxDeep: options.deep ? parseInt(options.deep, 10) : -1,
+});
 
 function callback(
     filePath: string,
@@ -78,7 +88,6 @@ function callback(
             stat.isFile() &&
             ['js', 'ts', 'jsx', 'tsx'].includes(getEndWith(filePath))
         ) {
-            const outFilePath = path.resolve(target, 'docs');
             transformDocs(filePath, outFilePath);
         }
     }
